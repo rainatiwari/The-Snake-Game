2e051c95aad6916b554f179f966b4de18fa9fa34
rainatiwari <66194815+rainatiwari@users.noreply.github.com>
2021-08-07 18:29:16 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -4,10 +4,14 @@ ALIGNMENT = "center"
 FONT = ("Courier", 15, "normal")
 
 
+
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 265)
@@ -15,13 +19,24 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score : {self.high_score}  ", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER !!! ", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER !!! ", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.update_high_score()
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_high_score(self):
+        with open("data.txt", mode="w") as file_write:
+            file_write.write(f"{self.high_score}")
